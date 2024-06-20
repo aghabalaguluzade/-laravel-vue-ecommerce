861ab694320c70dd8558da6ced75e3e0df9ed99f
aghabalaguluzade <aghabalaguluzade@gmail.com>
2024-06-20 15:13:14 +0400

    add more statuses in OrderStatus enum

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -6,5 +6,18 @@ enum OrderStatus: string
 {
     case Unpaid = 'unpaid';
     case Paid = 'paid';
+    case Cancelled = 'cancelled';
+    case Shipping = 'shipped';
     case Completed = 'completed';
+
+    public static function getStatuses()
+    {
+        return [
+            self::Paid,
+            self::Unpaid,
+            self::Cancelled,
+            self::Shipping,
+            self::Completed
+        ];
+    }
 }
